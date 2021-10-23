f2a4c72b5ba2749ff9908af7284131086d09ac9e
Mauricio <mauriciovieira8@hotmail.com>
2021-10-23 09:15:43 -0300

    Construtores, classe

---
+++ Capitulo8/Capitulo8/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capitulo8
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Rg { get; set; }
+        public double Cpf { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Cliente(string nome, int idade, int rg, double cpf)
+        {
+            Nome = nome;
+            Idade = Idade;
+            Rg = rg;
+            Cpf = cpf;
+
+            
+        }
+    }
+}
---
+++ Capitulo8/Capitulo8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Capitulo8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente Guilherme = new Cliente("Guilherme");
+            Guilherme.Nome = "Guilherme";
+            Guilherme.Idade = 21;
+            Guilherme.Cpf = 123456789;
+            Guilherme.Rg = 334455;
+
+            Console.WriteLine(" NOme: "+Guilherme.Nome+"\n Idade: "+Guilherme.Idade
+                +"\n CPF: "+Guilherme.Cpf+"\n RG: "+Guilherme.Rg);
+
+        }
+    }
+}

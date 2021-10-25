af3011c5de3d8ee2c2ee730f5d31932b95a2c808
Mauricio <mauriciovieira8@hotmail.com>
2021-10-25 14:54:21 -0300

    Estruturas de repetição For, While

---
+++ Capitulo5/Capitulo5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Capitulo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorInvestido = 1000.00;
+
+            for (int i = 1; i<=12;i++)
+            {
+                valorInvestido = valorInvestido * 1.01;
+            }
+            Console.WriteLine("Valor investido usando o For: "+valorInvestido.ToString("F2"));
+            Console.WriteLine();
+
+            double valorInvestido2 = 1000.00;
+            int i2 = 1;
+
+            while (i2 <=  12)
+            {
+                valorInvestido2 = valorInvestido2 * 1.01;
+                i2 += 1;
+                Console.WriteLine("Valor investido2 usando While: " + valorInvestido2.ToString("F2"));
+            }
+            
+        }
+    }
+}
---
+++ Capitulo5/OutrosExemplos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OutrosExemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 2;
+            for (int i = 0; i < 5; i += 1)
+            {
+                total = total * 2;
+            }
+            Console.WriteLine(total);
+
+            
+        }
+    }
+}
